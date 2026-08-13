313aa3efecf4620512213fcd74e0c696eefe9292
Madan Gouda <madan21@Madanas-MacBook-Air.local>
2026-08-14 00:44:53 +0530

    Configure H2 database for tests

--- backend/auth-service/src/test/java/com/enterprise/auth_service/AuthServiceApplicationTests.java
+++ backend/auth-service/src/test/java/com/enterprise/auth_service/AuthServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.enterprise.auth_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = AuthServiceApplication.class)
+@SpringBootTest
+@ActiveProfiles("test")
 class AuthServiceApplicationTests {
 
 	@Test
